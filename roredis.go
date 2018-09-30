1f6380dfc68e08e37d654028aa76e34a1fa7cebf
rohanthewiz@gmail.com <rohanthewiz@gmail.com>
2018-09-29 21:33:27 -0500

    Add bare minimum commands and tests

---
+++ roredis.go
@@ -0,0 +1,66 @@
+package roredis
+
+import (
+	"fmt"
+	"github.com/go-redis/redis" // (https://github.com/go-redis/redis)
+	"errors"
+	"time"
+)
+
+var rclient *redis.Client
+
+const defaultPort = "6379"
+const defaultHost = "localhost"
+
+type RedisCfg struct {
+	Host, Port, Password string
+	DB int
+}
+
+// The internal redis client must be initialized before other functions called
+func InitRedis(cfg RedisCfg) {
+	var host, port string
+
+	if cfg.Host == "" { host = defaultHost }
+	if cfg.Port == "" { port = defaultPort }
+
+	rclient = redis.NewClient(&redis.Options{
+		Addr: host + ":" + port,
+		Password: cfg.Password,
+		DB: cfg.DB, // 0 happens to be the default DB
+	})
+}
+
+func Ping() string {
+	if rclient == nil { return "" }
+	pong, err := rclient.Ping().Result()
+	if err != nil {
+		fmt.Println("Redis ping failed")
+		return ""
+	}
+	return pong
+	//	cl := Redis().LRange("testkey", 0, -1)
+}
+
+// Set expiration time to zero for no expiration
+func Set(key, value string, expiration time.Duration) error {
+	if rclient == nil { return errors.New("redis client not initialized - call InitRedis first") }
+	return rclient.Set(key, value, expiration).Err()
+}
+
+func Get(key string) (val string, err error) {
+	if rclient == nil { return val, errors.New("redis client not initialized - call InitRedis first") }
+
+	val, err = rclient.Get(key).Result()
+	if err == redis.Nil {
+		return "", errors.New("Key does not exist")
+	} else if err != nil {
+		return "", err
+	}
+	return val, err
+}
+
+func Del(key string) error {
+	if rclient == nil { return errors.New("redis client not initialized - call InitRedis first") }
+	return rclient.Del(key).Err()
+}
---
+++ roredis_test.go
@@ -0,0 +1,62 @@
+package roredis
+
+import "testing"
+
+const testKey = "tkey"
+const testBogusKey = "bogusKey"
+const testVal1 = "abc123"
+
+var testCfg = RedisCfg{
+	Host: "localhost",
+	DB: 0,
+}
+
+func TestInitRedis(t *testing.T) {
+	InitRedis(testCfg)
+	if rclient == nil {
+		t.Fatal("Failed to initialize Redis client")
+	}
+}
+
+func TestPing(t *testing.T) {
+	InitRedis(testCfg)
+	ret := Ping()
+	t.Log("Redis Ping returned", ret)
+}
+
+func TestSet(t *testing.T) {
+	InitRedis(testCfg)
+	err := Set(testKey, testVal1, 0)
+	if err != nil {
+		t.Error("Set failed", err)
+	}
+}
+
+func TestGetExistent(t *testing.T) {
+	InitRedis(testCfg)
+	ret, err := Get(testKey)
+	if err != nil {
+		t.Error("Get failed", err)
+		return
+	}
+	if ret != testVal1 {
+		t.Error("Get: returned value did not match Set value")
+	}
+}
+
+func TestGetNonExistent(t *testing.T) {
+	InitRedis(testCfg)
+	_, err := Get(testBogusKey)
+	if err != nil {
+		t.Log("Get failed for non-existent key", err)
+		return
+	}
+}
+
+func TestDel(t *testing.T) {
+	InitRedis(testCfg)
+	err := Del(testKey)
+	if err != nil {
+		t.Error("Delete failed", err)
+	}
+}
